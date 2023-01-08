7c999a5f866dcc30192f6537a2c8a952391c0968
Easton Elliott <easton@geekness.eu>
2023-01-07 20:15:05 -0700

    Add Walmart data parser

--- product_data_parser.py
+++ product_data_parser.py
@@ -22,6 +22,7 @@ class ProductDataParser:
                 "name": product_info["name"],
                 "price": product_info["price"]["current"]["amount"],
                 "image": product_info["image"]['src'],
+                #"size": product_info["size"]["value"],
                 "unit": product_info["price"]["unit"]["label"]
             }
             result.append(product_info_map)
@@ -36,6 +37,20 @@ class ProductDataParser:
                 "quantity": product_code["unitOfSize"]["size"],
                 "unit": product_code["unitOfSize"]["type"],
                 "image": product_code["image"]["default"]
+            }
+            result.append(product_info_map)
+        return result
+
+    def parse_walmart_json_data(data):
+        product_data = data['data']['search']['searchResult']['itemStacks'][0]['itemsV2']
+        result = []
+        for product_code in product_data:
+            product_info_map = {
+                "name": product_code["name"],
+                "price": product_code["priceInfo"]["currentPrice"]["price"],
+                "quantity": product_code["priceInfo"]["unitPrice"]["priceString"],
+                "unit": product_code["salesUnitType"],
+                "image": product_code["imageInfo"]["allImages"][0]['url']
             }
             result.append(product_info_map)
         return result
