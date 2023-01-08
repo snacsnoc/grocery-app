6fa0d4d95d4e05110bdb5db2975c0419213c7e18
Easton Elliott <easton@geekness.eu>
2023-01-07 21:07:50 -0700

    Update Walmart querying, add default price and quantity

--- product_data_parser.py
+++ product_data_parser.py
@@ -9,7 +9,7 @@ class ProductDataParser:
                 "price": product_code["prices"]["price"]["value"],
                 "quantity": product_code["prices"]["price"]["quantity"],
                 "unit": product_code["prices"]["price"]["unit"],
-                "image":product_code["imageAssets"][0]["mediumUrl"]
+                "image": product_code["imageAssets"][0]["mediumUrl"],
             }
             result.append(product_info_map)
         return result
@@ -21,12 +21,13 @@ class ProductDataParser:
             product_info_map = {
                 "name": product_info["name"],
                 "price": product_info["price"]["current"]["amount"],
-                "image": product_info["image"]['src'],
-                #"size": product_info["size"]["value"],
-                "unit": product_info["price"]["unit"]["label"]
+                "image": product_info["image"]["src"],
+                # "size": product_info["size"]["value"],
+                "unit": product_info["price"]["unit"]["label"],
             }
             result.append(product_info_map)
         return result
+
     def parse_saveonfoods_json_data(data):
         product_data = data["products"]
         result = []
@@ -36,21 +37,34 @@ class ProductDataParser:
                 "price": product_code["priceNumeric"],
                 "quantity": product_code["unitOfSize"]["size"],
                 "unit": product_code["unitOfSize"]["type"],
-                "image": product_code["image"]["default"]
+                "image": product_code["image"]["default"],
             }
             result.append(product_info_map)
         return result
 
     def parse_walmart_json_data(data):
-        product_data = data['data']['search']['searchResult']['itemStacks'][0]['itemsV2']
+        product_data = data["data"]["search"]["searchResult"]["itemStacks"][0][
+            "itemsV2"
+        ]
         result = []
         for product_code in product_data:
+
+            if product_code["priceInfo"]["unitPrice"] != None:
+                quantity = product_code["priceInfo"]["unitPrice"]["priceString"]
+            else:
+                quantity = "no"
+
+            if product_code["priceInfo"]["currentPrice"] != None:
+                price = product_code["priceInfo"]["currentPrice"]["price"]
+            else:
+                price = "no"
+
             product_info_map = {
                 "name": product_code["name"],
-                "price": product_code["priceInfo"]["currentPrice"]["price"],
-                "quantity": product_code["priceInfo"]["unitPrice"]["priceString"],
+                "price": price,
+                "quantity": quantity,
                 "unit": product_code["salesUnitType"],
-                "image": product_code["imageInfo"]["allImages"][0]['url']
+                "image": product_code["imageInfo"]["allImages"][0]["url"],
             }
             result.append(product_info_map)
         return result
