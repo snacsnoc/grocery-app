4724dc8d3842af3ad27e9028290a5d92da5f4c61
Easton Elliott <easton@geekness.eu>
2023-01-08 22:22:30 -0700

    Rewrite app.py, move error checking

--- app.py
+++ app.py
@@ -31,105 +31,111 @@ def index():
 @app.route("/search", methods=("GET", "POST"))
 def search():
     print(request)
-    if "query" and "postal_code" in request.form:
-        query = request.form["query"]
-        postal_code = request.form["postal_code"]
+    if "query" not in request.form or "postal_code" not in request.form:
+        raise ValueError("Missing query or postal_code in request.form")
 
-        # Look up users postal code, convert to lat & long to search for user's local stores
-        # Get each store's normalized data using ProductDataParser
-        products_data = SupermarketAPI(query)
-        parser = ProductDataParser
+    query = request.form["query"]
+    postal_code = request.form["postal_code"]
 
-        if postal_code is None:
-            raise Exception("Postal code is required")
+    # Look up users postal code, convert to lat & long to search for user's local stores
+    # Get each store's normalized data using ProductDataParser
+    products_data = SupermarketAPI(query)
+    parser = ProductDataParser
 
-        if DEBUG == "TRUE":
-            longitude = "-115.69"
-            latitude = "49.420"
-        else:
+    if postal_code is None:
+        raise Exception("Postal code is required")
+
+    # If DEBUG is "TRUE", set longitude and latitude to default values
+    if DEBUG == "TRUE":
+        longitude = "-115.69"
+        latitude = "49.420"
+    else:
+        # Attempt to get longitude, latitude, and formatted_address by looking up the postal code
+        try:
             longitude, latitude, formatted_address = lookup_postal_code_oc(postal_code)
+        except Exception as e:
+            print(f"Error looking up postal code: {e}")
+            raise
 
-        if latitude is None:
-            raise Exception("No geo coords!")
+    # If latitude is None, raise an exception
+    if latitude is None:
+        raise Exception("No geo coords!")
 
-        d = products_data.search_stores_pc(
-            latitude, longitude, store_brand="superstore"
-        )
-        e = products_data.search_stores_saveon(latitude, longitude)
+    d = products_data.search_stores_pc(latitude, longitude, store_brand="superstore")
+    e = products_data.search_stores_saveon(latitude, longitude)
 
-        pc_store_id = d["ResultList"][0]["Attributes"][0]["AttributeValue"]
-        saveon_store_id = e["items"][0]["retailerStoreId"]
-        walmart_store = products_data.search_stores_walmart(postal_code)
+    pc_store_id = d["ResultList"][0]["Attributes"][0]["AttributeValue"]
+    saveon_store_id = e["items"][0]["retailerStoreId"]
+    walmart_store = products_data.search_stores_walmart(postal_code)
 
-        # Set default stores (closest store)
-        products_data.set_store_pc(pc_store_id)
-        products_data.set_store_saveon(saveon_store_id)
-        products_data.set_store_walmart(latitude, longitude, postal_code)
+    # Set default stores (closest store)
+    products_data.set_store_pc(pc_store_id)
+    products_data.set_store_saveon(saveon_store_id)
+    products_data.set_store_walmart(latitude, longitude, postal_code)
 
-        # Set up a list of functions to send requests to
-        functions = [
-            products_data.query_saveon,
-            products_data.query_pc,
-            products_data.query_safeway,
-            products_data.query_walmart,
-        ]
+    # Set up a list of functions to send requests to
+    functions = [
+        products_data.query_saveon,
+        products_data.query_pc,
+        products_data.query_safeway,
+        products_data.query_walmart,
+    ]
 
-        # Use a ThreadPoolExecutor to send the requests in parallel
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            # Start the load operations and mark each future with its function
-            future_to_function = {executor.submit(func): func for func in functions}
-            results = {}
-            for future in concurrent.futures.as_completed(future_to_function):
-                func = future_to_function[future]
-                try:
-                    result = future.result()
-                except Exception as exc:
-                    print(f"Function {func.__name__} generated an exception: {exc}")
-                    results[func.__name__] = exc
-                else:
-                    # print(f"Function {func.__name__} returned result: {result}")
-                    results[func.__name__] = result
+    # Use a ThreadPoolExecutor to send the requests in parallel
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Start the load operations and mark each future with its function
+        future_to_function = {executor.submit(func): func for func in functions}
+        results = {}
+        for future in concurrent.futures.as_completed(future_to_function):
+            func = future_to_function[future]
+            try:
+                result = future.result()
+            except Exception as exc:
+                print(f"Function {func.__name__} generated an exception: {exc}")
+                results[func.__name__] = exc
+            else:
+                # print(f"Function {func.__name__} returned result: {result}")
+                results[func.__name__] = result
 
-        a = results["query_pc"]
-        c = results["query_saveon"]
-        b = results["query_safeway"]
-        f = results["query_walmart"]
+    a = results["query_pc"]
+    c = results["query_saveon"]
+    b = results["query_safeway"]
+    f = results["query_walmart"]
 
-        # Check if we have results for all stores
-        # TODO: rewrite this
-        if all([a, b, c, f]):
+    # Check if we have results for all stores
+    # TODO: rewrite this
+    if not all([a, b, c, f]):
+        search_data = {
+            "error": "No results",
+            "coords": {"latitude": latitude, "longitude": longitude},
+            "debug_mode": DEBUG,
+        }
+    else:
+        search_data = {
+            "store_name": {
+                "pc": d["ResultList"][0]["Name"],
+                "saveon": e["items"][0]["name"],
+                "safeway": "safewaySTORENAME",
+                "walmart": str(walmart_store["payload"]["stores"][0]["id"])
+                + " - "
+                + walmart_store["payload"]["stores"][0]["displayName"],
+            },
+            "results": {
+                "safeway": parser.parse_safeway_json_data(b),
+                "saveon": parser.parse_saveonfoods_json_data(c),
+                "pc": parser.parse_pc_json_data(a),
+                "walmart": parser.parse_walmart_json_data(f),
+            },
+            "coords": {
+                "latitude": latitude,
+                "longitude": longitude,
+                "postal_code": postal_code,
+                "formatted_address": formatted_address,
+            },
+            "debug_mode": DEBUG,
+        }
 
-            search_data = {
-                "store_name": {
-                    "pc": d["ResultList"][0]["Name"],
-                    "saveon": e["items"][0]["name"],
-                    "safeway": "safewaySTORENAME",
-                    "walmart": str(walmart_store["payload"]["stores"][0]["id"])
-                    + " - "
-                    + walmart_store["payload"]["stores"][0]["displayName"],
-                },
-                "results": {
-                    "safeway": parser.parse_safeway_json_data(b),
-                    "saveon": parser.parse_saveonfoods_json_data(c),
-                    "pc": parser.parse_pc_json_data(a),
-                    "walmart": parser.parse_walmart_json_data(f),
-                },
-                "coords": {
-                    "latitude": latitude,
-                    "longitude": longitude,
-                    "postal_code": postal_code,
-                    "formatted_address": formatted_address,
-                },
-                "debug_mode": DEBUG,
-            }
-        else:
-            search_data = {
-                "error": "No results",
-                "coords": {"latitude": latitude, "longitude": longitude},
-                "debug_mode": DEBUG,
-            }
-
-        return render_template("search.html", result_data=search_data)
+    return render_template("search.html", result_data=search_data)
 
 
 # Look up postal code to lat,long coords
